bc74c98a9fa0ced864589671826d8315c05a8b3a
Miller <miller.yu@leandev.se>
2018-05-21 15:15:21 +0800

    update public_api and package.json of ng-busy

--- projects/ng-busy/src/public_api.ts
+++ projects/ng-busy/src/public_api.ts
@@ -2,13 +2,6 @@
  * Public API Surface of ng-busy
  */
 
-import {NgBusyDirective} from './lib/ng-busy.directive';
-
 export * from './lib/ng-busy.module';
-
+export * from './lib/model/busy-config';
 export { NgBusyDirective } from './lib/ng-busy.directive';
-export { BusyConfig } from './lib/model/busy-config';
-export { BUSY_CONFIG_DEFAULTS } from './lib/model/busy-config';
-export { IBusyConfig } from './lib/model/busy-config';
-export { DefaultBusyComponent } from './lib/model/busy-config';
-
