cac60c9ac9a695fd929833d001cb4bc5283e627d
Miller <miller.yu@leandev.se>
2018-08-29 13:35:18 +0800

    improve

--- projects/ng-busy/src/public_api.ts
+++ projects/ng-busy/src/public_api.ts
@@ -2,8 +2,6 @@
  * Public API Surface of ng-busy
  */
 
-import {IBusyConfig} from './lib/model/busy-config';
-
 export * from './lib/ng-busy.module';
-export {IBusyConfig} from './lib/model/busy-config';
+export * from './lib/model/busy-config';
 export {NgBusyDirective} from './lib/ng-busy.directive';
