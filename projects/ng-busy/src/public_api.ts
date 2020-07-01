60330e372af36a6f449e8993072d2f02e399c921
THOURA OUSSAMA <o.thoura@firethunder.ma>
2020-07-01 14:55:58 +0100

    Angular 10 build

--- projects/ng-busy/src/public_api.ts
+++ projects/ng-busy/src/public_api.ts
@@ -2,7 +2,9 @@
  * Public API Surface of ng-busy
  */
 
-export * from './lib/ng-busy.module';
-export * from './lib/model/busy-config';
-export {NgBusyDirective} from './lib/ng-busy.directive';
-export {InstanceConfigHolderService} from './lib/service/instance-config-holder.service';
+export * from "./lib/ng-busy.module";
+export * from "./lib/util/isPromise";
+export * from "./lib/model/busy-config";
+export { NgBusyDirective } from "./lib/ng-busy.directive";
+export { NgBusyComponent } from "./lib/component/ng-busy/ng-busy.component";
+export { InstanceConfigHolderService } from "./lib/service/instance-config-holder.service";
