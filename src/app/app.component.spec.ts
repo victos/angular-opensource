31ec3a35156072173eae47998b30d4abd136d1ab
THOURA OUSSAMA <o.thoura@firethunder.ma>
2020-11-12 18:05:36 +0100

    Angular 11 build

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,10 @@
-import {TestBed, async} from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
 import {NgBusyDemoModule} from './modules/ng-busy-demo/ng-busy-demo.module';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
@@ -15,12 +15,12 @@ describe('AppComponent', () => {
       ]
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+  it('should create the app', waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'app'`, waitForAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
--- src/app/modules/ng-busy-demo/component/custom-busy-component/custom-busy-component.component.spec.ts
+++ src/app/modules/ng-busy-demo/component/custom-busy-component/custom-busy-component.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import {CustomBusyComponentComponent} from './custom-busy-component.component';
 
@@ -6,7 +6,7 @@ describe('CustomBusyComponentComponent', () => {
   let component: CustomBusyComponentComponent;
   let fixture: ComponentFixture<CustomBusyComponentComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [CustomBusyComponentComponent],
       providers: [{provide: 'instanceConfigHolder', useValue: {config: {message : 'hello'}}}]
